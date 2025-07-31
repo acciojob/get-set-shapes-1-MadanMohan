c2a6b04a8988fe10a99b30c02387d7475f1ba25c
Madan Mohan Reddy <119772090+1-MadanMohan@users.noreply.github.com>
2025-07-31 16:42:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ class Rectangle {
 		this._height = height;
 	}
 
-	area() {
+	getArea() {
 		return this._width * this._height;
 	}
 
@@ -19,11 +19,16 @@ class Rectangle {
 
 class Square extends Rectangle {
 	constructor(side) {
-		super(side, side); // A square has equal width and height
+		super(side, side); // both width and height are equal
 	}
 
 	getPerimeter() {
-		return 4 * this.side;
+		return 4 * this._width;
+	}
+
+	getArea() {
+		// Optional: override to clarify intention, but base class already works
+		return this._width * this._height;
 	}
 }
 
