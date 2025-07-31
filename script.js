b407ad15ddf7fc8f500cd83b2fd349dd5e757857
Madan Mohan Reddy <119772090+1-MadanMohan@users.noreply.github.com>
2025-07-31 16:40:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,31 @@
-//complete this code
-class Rectangle {}
+class Rectangle {
+	constructor(width, height) {
+		this._width = width;
+		this._height = height;
+	}
 
-class Square extends Animal {}
+	area() {
+		return this._width * this._height;
+	}
+
+	get height() {
+		return this._height;
+	}
+
+	get width() {
+		return this._width;
+	}
+}
+
+class Square extends Rectangle {
+	constructor(side) {
+		super(side, side); // A square has equal width and height
+	}
+
+	getPerimeter() {
+		return 4 * this.side;
+	}
+}
 
 // Do not change the code below this line
 window.Rectangle = Rectangle;
